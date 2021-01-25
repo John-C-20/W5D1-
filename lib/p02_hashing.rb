c07d595b7ede36e3d87ca3edb5f5a6c898efd309
Solomon Bassalian <solomonbassalian@gmail.com>
2021-01-25 17:27:59 -0500

    pledging

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    id = 0
+    self.each.with_index do |ele, i|
+      id += i.hash * ele.hash
+    end 
+    id 
+
   end
+
 end
 
 class String
   def hash
+    abc = ('a'..'z').to_a
+    id = 0
+    self.each_char.with_index do |ele,i|
+      id += i.hash * abc.index(ele).hash 
+    end 
+    id
   end
 end
 
@@ -16,6 +29,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    
+    id = 0
+    self.each do |k,v|
+      id += k.hash * v.hash
+    end 
+    id
   end
 end
--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -7,12 +7,25 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count == num_buckets
+    bucket = key.hash % num_buckets
+    if !self.include?(key)
+      @store[bucket] << key 
+      @count += 1
+    end
   end
 
   def include?(key)
+    bucket = key.hash % num_buckets
+    @store[bucket].include?(key)
   end
 
   def remove(key)
+    bucket = key.hash % num_buckets
+    if include?(key)
+      @store[bucket].delete(key) 
+      @count -= 1 
+    end
   end
 
   private
@@ -26,5 +39,11 @@ class HashSet
   end
 
   def resize!
+    numbers = @store.flatten 
+    @store = Array.new(num_buckets*2) {Array.new}
+    numbers.each do |number|
+      bucket = number.hash % num_buckets
+      @store[bucket] << number 
+    end
   end
 end
